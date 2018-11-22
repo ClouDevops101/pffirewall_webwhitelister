1143fc29a3b8fd5e3ccf2cd97120dccd86c1ff8f
LinuxArchitects <heddar.abdelilah@live.fr>
2018-11-22 16:33:47 +0100

    Adding Resolution if no arg set

--- pffirewall_webwhitelister.py
+++ pffirewall_webwhitelister.py
@@ -5,6 +5,7 @@ import os
 import socket
 import re
 
+#IP resolution
 def lookup_ip(addr):
    try:
        return socket.gethostbyaddr(addr)[0]
@@ -16,6 +17,8 @@ def lookup_host(host):
    try:
        #return socket.gethostbyname(host)
        return socket.gethostbyname_ex(host)[2:][0]
+       #socket.gethostbyname_ex("caf.fr")
+       #('caf.fr', [], ['195.101.92.10', '77.158.24.239'])
    except socket.gaierror:
        return None
 
@@ -37,12 +40,15 @@ def main():
 
   addressToAdd=[]
   if len(sys.argv) == 2:
+      # pffirewall_webwhitelister.py http://12factor.net
+
       r1 = re.findall(r"^[a-z][a-z0-9+\-.]*://([a-z0-9\-._~%!$&'()*+,;=]+@)?([a-z0-9\-._~%]+|\[[a-z0-9\-._~%!$&'()*+,;=:]+\])",comment)
       domain = r1[0][1]
       addressToAdd = (lookup_host(domain))
       print "[" + domain + "]"
       comment = domain
   elif len(sys.argv) > 2:
+      # pffirewall_webwhitelister.py http://12factor.net 52.71.139.107 52.45.248.161
       addressToAdd = sys.argv[2:]
   else:
       sys.exit(0)
@@ -53,8 +59,20 @@ def main():
       contents = f.read()
       # TODO : Add regex to not match 84.4.4.44 against 4.4.4.4
       for ip  in addressToAdd:
+          # Ip not registred and a valid ip
           if ip not in contents:
-              list_ip.append(ip)  ## app
+              try:
+                  socket.inet_aton(ip)
+                  list_ip.append(ip)
+                  continue ## app
+              except socket.error:
+                  print "not ipv4 address"
+              try:
+                  socket.inet_pton(socket.AF_INET6,ip)
+                  list_ip.append(ip)
+                  continue
+              except socket.error:
+                  print "not ipv6 address"
           else:
               print ip + " Already registred"
   if list_ip:
