11f07984c1fa6b9a1a28417acdc0494b5e5d5c7c
LinuxArchitects <heddar.abdelilah@live.fr>
2018-11-22 16:48:59 +0100

    Adding IPV6 compatibility

--- pffirewall_webwhitelister.py
+++ pffirewall_webwhitelister.py
@@ -40,13 +40,20 @@ def main():
 
   addressToAdd=[]
   if len(sys.argv) == 2:
-      # pffirewall_webwhitelister.py http://12factor.net
+      # Handle
+      # pffirewall_webwhitelister.py http://12factor.net/frfrfrfrfr
+      try:
+        r1 = re.findall(r"^[a-z][a-z0-9+\-.]*://([a-z0-9\-._~%!$&'()*+,;=]+@)?([a-z0-9\-._~%]+|\[[a-z0-9\-._~%!$&'()*+,;=:]+\])",comment)
+        domain = r1[0][1]
+        addressToAdd = (lookup_host(domain))
+        comment = domain
+        print "[" + domain + "]"
+      except:
+        # Handle
+        # pffirewall_webwhitelister.py 12factor.net
+        print "not a url"
+        addressToAdd = (lookup_host(comment))
 
-      r1 = re.findall(r"^[a-z][a-z0-9+\-.]*://([a-z0-9\-._~%!$&'()*+,;=]+@)?([a-z0-9\-._~%]+|\[[a-z0-9\-._~%!$&'()*+,;=:]+\])",comment)
-      domain = r1[0][1]
-      addressToAdd = (lookup_host(domain))
-      print "[" + domain + "]"
-      comment = domain
   elif len(sys.argv) > 2:
       # pffirewall_webwhitelister.py http://12factor.net 52.71.139.107 52.45.248.161
       addressToAdd = sys.argv[2:]
@@ -62,17 +69,21 @@ def main():
           # Ip not registred and a valid ip
           if ip not in contents:
               try:
+                  # Handle
+                  # pffirewall_webwhitelister.py  https://www.oreilly.com/ 1080:0:0:0:8:800:200C:417A
                   socket.inet_aton(ip)
                   list_ip.append(ip)
                   continue ## app
               except socket.error:
-                  print "not ipv4 address"
+                  print "[WARN] : Not ipv4 address"
               try:
+                  # Handle :
+                  # pffirewall_webwhitelister.py  https://www.oreilly.com/ 1.1.1.3
                   socket.inet_pton(socket.AF_INET6,ip)
                   list_ip.append(ip)
                   continue
               except socket.error:
-                  print "not ipv6 address"
+                  print "[WARN] : Not ipv6 address"
           else:
               print ip + " Already registred"
   if list_ip:
